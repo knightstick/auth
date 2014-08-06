0d271530532a70bbb15cfe8a23846f3091c8a328
Chris Jewell <chrisjohnjewell@gmail.com>
2014-08-06 15:15:23 +1000

    Add users, sessions and log in/out mechanism

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get "log_in" => 'sessions#new', as: "log_in"
+  get "log_out" => 'sessions#destroy', as: "log_out"
+
+  get "sign_up" => 'users#new', as: "sign_up"
+  root 'users#new'
+  resources :users
+  resources :sessions
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
